bd45d7a04745f646cdbf3150a6301b5ee38ef087
devashish <mshubh612@gmail.com>
2024-10-25 00:18:40 +0530

    setup appwrite

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  console.log(import.meta.env.VITE_APPWRITE_URL);     
+
+  return (
+    <>
+    <h1>DEvashish</h1>
+    
+    
+    </>
+  )
+}
+
+export default App
