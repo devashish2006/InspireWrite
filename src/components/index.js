b9c0fb3f0319b5bd60d9a4962aa4e2e5bd38b9e2
devashish <mshubh612@gmail.com>
2024-10-30 18:44:49 +0530

    add login and signup component

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,15 @@ import Footer from "./footer/footer";
 import Container from "../container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./header/LogoutBtn";
+import Button from "./Button"
+import Input from "./input";
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Button,
+    Input
 }
